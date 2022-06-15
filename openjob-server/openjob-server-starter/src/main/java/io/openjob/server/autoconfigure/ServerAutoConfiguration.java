c75086efbc46f67009f25b9c65b1cfa38c30416f
stelin <794774870@qq.com>
2022-06-15 19:30:46 +0800

    Add cluster and core module

---
+++ openjob-server/openjob-server-starter/src/main/java/io/openjob/server/autoconfigure/ServerAutoConfiguration.java
@@ -0,0 +1,12 @@
+package io.openjob.server.autoconfigure;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author stelin <swoft@qq.com>
+ * @since 1.0.0
+ */
+@Configuration
+public class ServerAutoConfiguration {
+    
+}
