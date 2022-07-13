fc67e6c0bb6ee3de6394782c18fb40dc1b3e0e1d
stelin <794774870@qq.com>
2022-07-13 11:39:30 +0800

    Add junit

---
+++ openjob-worker/openjob-worker-core/src/test/java/io/openjob/worker/config/OpenjobConfigTest.java
@@ -0,0 +1,19 @@
+package io.openjob.worker.config;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author stelin <swoft@qq.com>
+ * @since 1.0.0
+ */
+public class OpenjobConfigTest {
+    @Test
+    public void testGetConfig() {
+        Assertions.assertEquals(OpenjobConfig.getConfig("openjob.worker.test"), "test");
+        Assertions.assertEquals(OpenjobConfig.getString("openjob.worker.test"), "test");
+
+        Assertions.assertEquals(OpenjobConfig.getInteger("openjob.worker.int.value"), 1);
+        Assertions.assertEquals(OpenjobConfig.getLong("openjob.worker.long.value"), 1L);
+    }
+}
