001301ecafac077265d930d341eb7cb20c500aff
stelin <794774870@qq.com>
2022-09-17 17:59:45 +0800

    add

---
+++ openjob-server/openjob-server-repository/src/main/java/io/openjob/server/repository/dao/impl/JobInstanceTaskDAOImpl.java
@@ -0,0 +1,10 @@
+package io.openjob.server.repository.dao.impl;
+
+import io.openjob.server.repository.dao.JobInstanceTaskDAO;
+
+/**
+ * @author stelin <swoft@qq.com>
+ * @since 1.0.0
+ */
+public class JobInstanceTaskDAOImpl implements JobInstanceTaskDAO {
+}
---
+++ openjob-server/openjob-server-repository/src/main/java/io/openjob/server/repository/entity/JobInstanceTask.java
@@ -0,0 +1,57 @@
+package io.openjob.server.repository.entity;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * @author stelin <swoft@qq.com>
+ * @since 1.0.0
+ */
+@Data
+@Entity
+@Table(name = "job_instance_task")
+public class JobInstanceTask {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "job_id")
+    private Long jobId;
+
+    @Column(name = "job_instance_id")
+    private Long jobInstanceId;
+
+    @Column(name = "circle_id")
+    private Long circleId;
+
+    @Column(name = "task_id")
+    private String taskId;
+
+    @Column(name = "parent_task_id")
+    private String parentTaskId;
+
+    @Column(name = "task_name")
+    private String taskName;
+
+    @Column(name = "status")
+    private Integer status;
+
+    @Column(name = "result")
+    private String result;
+
+    @Column(name = "worker_address")
+    private String workerAddress;
+
+    @Column(name = "create_time")
+    private Integer createTime;
+
+    @Column(name = "update_time")
+    private Integer updateTime;
+}
