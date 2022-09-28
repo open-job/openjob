22421fc12a23409109714af89535b2cadfb3cf97
stelin <794774870@qq.com>
2022-09-28 17:42:11 +0800

    add log

---
+++ openjob-server/openjob-server-log/src/main/java/io/openjob/server/log/constant/LogJdbcDriverConstant.java
@@ -0,0 +1,10 @@
+package io.openjob.server.log.constant;
+
+/**
+ * @author stelin <swoft@qq.com>
+ * @since 1.0.0
+ */
+public class LogJdbcDriverConstant {
+    public final static String H2 = "org.h2.Driver";
+    public final static String MYSQL = "com.mysql.cj.jdbc.Driver";
+}
