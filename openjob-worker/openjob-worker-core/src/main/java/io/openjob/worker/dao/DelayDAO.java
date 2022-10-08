12bc41a48cd21ce68b5196a534d92cc9dcd0ea8e
stelin <794774870@qq.com>
2022-10-08 16:48:35 +0800

    add

--- openjob-worker/openjob-worker-core/src/main/java/io/openjob/worker/dao/DelayDAO.java
+++ openjob-worker/openjob-worker-core/src/main/java/io/openjob/worker/dao/DelayDAO.java
@@ -1,17 +1,69 @@
 package io.openjob.worker.dao;
 
+import io.openjob.common.util.DateUtil;
+import io.openjob.worker.entity.Delay;
 import io.openjob.worker.persistence.DelayPersistence;
 import io.openjob.worker.persistence.H2DelayMemoryPersistence;
+import lombok.extern.slf4j.Slf4j;
+
+import java.sql.SQLException;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * @author stelin <swoft@qq.com>
  * @since 1.0.0
  */
+@Slf4j
 public class DelayDAO {
     public final static DelayDAO INSTANCE = new DelayDAO();
     private final DelayPersistence delayPersistence;
 
     private DelayDAO() {
         this.delayPersistence = new H2DelayMemoryPersistence();
+    }
+
+    public Integer batchSave(List<Delay> delays) {
+        try {
+            int now = DateUtil.now();
+            delays.forEach(d -> {
+                d.setCreateTime(now);
+                d.setUpdateTime(now);
+            });
+
+            return this.delayPersistence.batchSave(delays);
+        } catch (SQLException e) {
+            log.error("Delay batch add failed!", e);
+            return 0;
+        }
+    }
+
+    public Integer batchUpdatePullTimeById(List<Delay> delays) {
+        try {
+            int now = DateUtil.now();
+            delays.forEach(d -> d.setUpdateTime(now));
+            return this.delayPersistence.batchUpdatePullTimeById(delays);
+        } catch (SQLException e) {
+            log.error("Delay batch update failed!", e);
+            return 0;
+        }
+    }
+
+    public List<Delay> findNotPullList() {
+        try {
+            return this.delayPersistence.findNotPullList();
+        } catch (SQLException e) {
+            log.error("Delay find not pull list failed!", e);
+            return Collections.emptyList();
+        }
+    }
+
+    public Integer deleteByIds(List<Long> ids) {
+        try {
+            return this.delayPersistence.deleteByIds(ids);
+        } catch (SQLException e) {
+            log.error("Delay delete ids failed!", e);
+            return 0;
+        }
     }
 }
--- openjob-worker/openjob-worker-core/src/main/java/io/openjob/worker/persistence/H2DelayMemoryPersistence.java
+++ openjob-worker/openjob-worker-core/src/main/java/io/openjob/worker/persistence/H2DelayMemoryPersistence.java
@@ -1,12 +1,16 @@
 package io.openjob.worker.persistence;
 
+import io.openjob.common.util.DateUtil;
 import io.openjob.worker.entity.Delay;
 import lombok.extern.slf4j.Slf4j;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * @author stelin <swoft@qq.com>
@@ -31,7 +35,7 @@ public class H2DelayMemoryPersistence implements DelayPersistence {
 
     @Override
     public void initTable() throws Exception {
-        String createSql = "CREATE TABLE IF NOT EXISTS `delay` (" +
+        String createSql = "CREATE TABLE IF NOT EXISTS `delay_worker` (" +
                 "  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY," +
                 "  `pull_time` int(11) NOT NULL DEFAULT '0'," +
                 "  `create_time` int(11) NOT NULL," +
@@ -47,21 +51,115 @@ public class H2DelayMemoryPersistence implements DelayPersistence {
 
     @Override
     public Integer batchSave(List<Delay> delays) throws SQLException {
-        return null;
+        String sql = "INSERT INTO delay_worker (" +
+                "id," +
+                "pull_time," +
+                "create_time," +
+                "update_time" +
+                ") VALUES (?, ?, ?, ?)";
+
+        PreparedStatement ps = null;
+        try (Connection connection = this.connectionPool.getConnection()) {
+            ps = connection.prepareStatement(sql);
+            connection.setAutoCommit(false);
+            for (Delay delay : delays) {
+                ps.setLong(1, delay.getId());
+                ps.setInt(2, delay.getPullTime());
+                ps.setInt(3, delay.getCreateTime());
+                ps.setInt(4, delay.getUpdateTime());
+                ps.addBatch();
+            }
+            int[] result = ps.executeBatch();
+            connection.commit();
+            connection.setAutoCommit(true);
+            ps.clearBatch();
+            return result.length;
+        } finally {
+            if (Objects.nonNull(ps)) {
+                ps.close();
+            }
+        }
     }
 
     @Override
     public Integer batchUpdatePullTimeById(List<Delay> delays) throws SQLException {
-        return null;
+        String sql = "UPDATE delay_worker SET pull_time=?, update_time=? WHERE id=?";
+
+        PreparedStatement ps = null;
+        try (Connection connection = this.connectionPool.getConnection()) {
+            connection.setAutoCommit(false);
+            ps = connection.prepareStatement(sql);
+            for (Delay delay : delays) {
+                ps.setInt(1, delay.getPullTime());
+                ps.setInt(2, delay.getUpdateTime());
+                ps.setLong(3, delay.getId());
+                ps.addBatch();
+            }
+
+            int[] result = ps.executeBatch();
+            connection.commit();
+            connection.setAutoCommit(true);
+            return result.length;
+        } finally {
+            if (Objects.nonNull(ps)) {
+                ps.close();
+            }
+        }
     }
 
     @Override
     public List<Delay> findNotPullList() throws SQLException {
-        return null;
+        ResultSet rs = null;
+        String sql = "SELECT id,pull_time FROM delay_worker WHERE pull_time > ?";
+        try (Connection connection = this.connectionPool.getConnection();
+             PreparedStatement ps = connection.prepareStatement(sql)) {
+            ps.setInt(1, DateUtil.now());
+            rs = ps.executeQuery();
+
+            List<Delay> delayList = new ArrayList<>();
+            while (rs.next()) {
+                delayList.add(convert(rs));
+            }
+            return delayList;
+        } finally {
+            if (Objects.nonNull(rs)) {
+                rs.close();
+            }
+        }
     }
 
     @Override
     public Integer deleteByIds(List<Long> ids) throws SQLException {
-        return null;
+        String sql = "DELETE FROM delay_worker WHERE id = ?";
+
+        PreparedStatement ps = null;
+        try (Connection connection = this.connectionPool.getConnection()) {
+            connection.setAutoCommit(false);
+            ps = connection.prepareStatement(sql);
+            for (Long id : ids) {
+                ps.setLong(1, id);
+                ps.addBatch();
+            }
+
+            int[] counts = ps.executeBatch();
+            connection.commit();
+            connection.setAutoCommit(true);
+            return counts.length;
+        } finally {
+            if (Objects.nonNull(ps)) {
+                ps.close();
+            }
+        }
+    }
+
+    /**
+     * @param rs result set
+     * @return Delay
+     */
+    private Delay convert(ResultSet rs) throws SQLException {
+        Delay delay = new Delay();
+        delay.setId(rs.getLong("id"));
+        delay.setPullTime(rs.getInt("pull_time"));
+        return delay;
     }
 }
