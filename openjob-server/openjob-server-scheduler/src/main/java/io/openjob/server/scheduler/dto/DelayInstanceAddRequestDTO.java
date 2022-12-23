c7a0f7e7a719b05b69dd5240fd535888fac34d51
stelin <794774870@qq.com>
2022-12-23 21:04:56 +0800

    :sparkles:new:json simple

--- openjob-server/openjob-server-scheduler/src/main/java/io/openjob/server/scheduler/dto/DelayInstanceAddRequestDTO.java
+++ openjob-server/openjob-server-scheduler/src/main/java/io/openjob/server/scheduler/dto/DelayInstanceAddRequestDTO.java
@@ -1,5 +1,6 @@
 package io.openjob.server.scheduler.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 /**
@@ -13,25 +14,30 @@ public class DelayInstanceAddRequestDTO {
      * Delay task unique id.
      * If is null or blank, will to auto generate.
      */
+    @JsonProperty(value = "ti")
     private String taskId;
 
     /**
      * Topic
      */
+    @JsonProperty(value = "t")
     private String topic;
 
     /**
      * Delay task params.
      */
+    @JsonProperty(value = "p")
     private String params;
 
     /**
      * Delay task extra params.
      */
+    @JsonProperty(value = "e")
     private String extra;
 
     /**
      * Delay task execute time.
      */
+    @JsonProperty(value = "et")
     private Long executeTime;
 }
