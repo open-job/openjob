0272204e5697f3424f4a0041cfcc4bee40aa5f4e
stelin <794774870@qq.com>
2023-05-13 10:28:52 +0800

    :art:modify args

--- openjob-server/openjob-server-starter/src/main/java/io/openjob/server/ServerApplication.java
+++ openjob-server/openjob-server-starter/src/main/java/io/openjob/server/ServerApplication.java
@@ -16,6 +16,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class ServerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ServerApplication.class);
+        SpringApplication.run(ServerApplication.class, args);
     }
 }
