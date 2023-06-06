34d354e77aa92594eea75ee9d09b729b1b0d9ea0
stelin <794774870@qq.com>
2023-06-06 22:35:41 +0800

    :zap:update statid

---
+++ openjob-server/openjob-server-starter/src/main/resources/static/assets/index.7b13d60d.js
@@ -0,0 +1,1 @@
+import{s as e,N as r}from"./request.be218ac0.js";function d(){return{getList:t=>e({url:r.list,method:"get",params:t}),add:t=>e({url:r.add,method:"post",data:t}),update:t=>e({url:r.update,method:"post",data:t}),updateStatus:t=>e({url:r.updateStatus,method:"post",data:t}),delete:t=>e({url:r.delete,method:"post",data:t})}}export{d as u};
