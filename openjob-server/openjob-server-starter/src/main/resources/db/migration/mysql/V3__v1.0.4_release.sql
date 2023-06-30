ca1e35dd636d7d0a866df660737224c1894d1cd8
stelin <794774870@qq.com>
2023-06-30 23:17:22 +0800

    :zap:add sql for mysql

---
+++ openjob-server/openjob-server-starter/src/main/resources/db/migration/mysql/V3__v1.0.4_release.sql
@@ -0,0 +1,14 @@
+# `admin_permission`
+# ------------------------------------------------------------
+ALTER TABLE `admin_permission` CHANGE `meta` `meta` TEXT  CHARACTER SET utf8mb4  COLLATE utf8mb4_general_ci  NOT NULL;
+
+# `admin_role`
+# ------------------------------------------------------------
+ALTER TABLE `admin_role` CHANGE `menu_ids` `menu_ids` TEXT  CHARACTER SET utf8mb4  COLLATE utf8mb4_general_ci  NOT NULL;
+ALTER TABLE `admin_role` CHANGE `perm_ids` `perm_ids` TEXT  CHARACTER SET utf8mb4  COLLATE utf8mb4_general_ci  NOT NULL;
+ALTER TABLE `admin_role` CHANGE `namespace_ids` `namespace_ids` TEXT  CHARACTER SET utf8mb4  COLLATE utf8mb4_general_ci  NOT NULL;
+ALTER TABLE `admin_role` CHANGE `app_ids` `app_ids` TEXT  CHARACTER SET utf8mb4  COLLATE utf8mb4_general_ci  NOT NULL;
+
+# `admin_user`
+# ------------------------------------------------------------
+ALTER TABLE `admin_user` CHANGE `role_ids` `role_ids` TEXT  CHARACTER SET utf8mb4  COLLATE utf8mb4_general_ci  NOT NULL;
