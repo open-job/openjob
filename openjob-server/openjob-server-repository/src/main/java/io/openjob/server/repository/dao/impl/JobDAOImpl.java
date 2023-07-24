d1343b7590a5afd80acd76c4d68871a33aa8dda1
stelin <794774870@qq.com>
2023-07-24 21:03:12 +0800

    :zap:add execute timeout

--- openjob-server/openjob-server-repository/src/main/java/io/openjob/server/repository/dao/impl/JobDAOImpl.java
+++ openjob-server/openjob-server-repository/src/main/java/io/openjob/server/repository/dao/impl/JobDAOImpl.java
@@ -88,6 +88,7 @@ public class JobDAOImpl implements JobDAO {
                     j.setStatus(job.getStatus());
                     j.setFailRetryInterval(job.getFailRetryInterval());
                     j.setFailRetryTimes(job.getFailRetryTimes());
+                    j.setExecuteTimeout(job.getExecuteTimeout());
                     j.setConcurrency(job.getConcurrency());
                     j.setTimeExpression(job.getTimeExpression());
                     j.setTimeExpressionType(job.getTimeExpressionType());
