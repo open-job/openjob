21f16cf4454e2042bba61b4953c4b595a99cbff7
stelin <794774870@qq.com>
2023-08-15 20:39:54 +0800

    :zap:add api

--- openjob-server/openjob-server-admin/src/main/java/io/openjob/server/admin/controller/JobInstanceTaskController.java
+++ openjob-server/openjob-server-admin/src/main/java/io/openjob/server/admin/controller/JobInstanceTaskController.java
@@ -1,8 +1,16 @@
 package io.openjob.server.admin.controller;
 
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author stelin swoft@qq.com
  * @since 1.0.7
  */
+@Api(value = "Instance task", tags = "Instance task")
+@RestController
+@RequestMapping("/admin/instance-task")
 public class JobInstanceTaskController {
+
 }
