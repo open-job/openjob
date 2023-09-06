3948e5a1930e5925af10dad2c62a4431e84b3e72
stelin <794774870@qq.com>
2023-09-06 19:49:28 +0800

    :bug:fixed sql

--- openjob-server/openjob-server-starter/src/main/resources/db/migration/mysql/V5__v1.0.7_release.sql
+++ openjob-server/openjob-server-starter/src/main/resources/db/migration/mysql/V5__v1.0.7_release.sql
@@ -3,8 +3,7 @@
 ALTER TABLE `job_instance`
     ADD `dispatch_version` bigint(20)  NOT NULL  DEFAULT '0'  COMMENT 'Dispatch version'  AFTER `last_report_time`;
 ALTER TABLE `job_instance`
-    ADD `execute_once` tinyint(2) NOT NULL DEFAULT '2' COMMENT 'Execute once, 1=yes 2=no' AFTER `execute_once`;
-
+    ADD `execute_once` tinyint(2) NOT NULL DEFAULT '2' COMMENT 'Execute once, 1=yes 2=no' AFTER `execute_type`;
 
 #`job_instance_task`
 # ------------------------------------------------------------
